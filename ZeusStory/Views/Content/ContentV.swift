0e1a21d972fccae0eba55d617a7334e40dd40cd0
Anton <danilovanton724@gmail.com>
2024-02-07 22:37:48 +0100

    new version app

---
+++ ZeusStory/Views/Content/ContentV.swift
@@ -0,0 +1,18 @@
+//
+//  ContentV.swift
+//  ZeusStory
+//
+//  Created by Anton on 7/2/24.
+//
+
+import SwiftUI
+
+struct ContentV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentV()
+}
---
+++ ZeusStory/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  ZeusStory
+//
+//  Created by Anton on 7/2/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
